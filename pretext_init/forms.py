3e118c32fa3674a119344cbb62916d5d54708bd8
Matthew Leingang <leingang@nyu.edu>
2019-06-19 16:14:53 -0700

    Add a radio button for license

--- pretext_init/forms.py
+++ pretext_init/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 
 class NewProjectForm(FlaskForm):
@@ -8,4 +8,9 @@ class NewProjectForm(FlaskForm):
     project_edition = StringField('Edition')
     project_copyright_year = StringField('Copyright Year')
     project_copyright_holder = StringField('Copyright Holder')
+    project_license = RadioField('License',choices=[
+        ('CC-BY-SA','Creative Commons Attribution-ShareAlike'),
+        ('CC-BY-SA-NC','Creative Commons Attribution-NonCommercial-ShareAlike'),
+        ('GFDL','GNU Free Documentation License')
+    ])
     submit = SubmitField('Generate')
