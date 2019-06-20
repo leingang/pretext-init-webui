3aae9e5cd72d4c6810345af7a1c2bcfc679c2ae2
Matthew Leingang <leingang@nyu.edu>
2019-06-20 16:46:35 -0700

    [WIP] Add form support for outline
    Not implemented on server side yet

--- pretext_init/forms.py
+++ pretext_init/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, RadioField, SubmitField, FieldList, FormField
+from wtforms import StringField, PasswordField, BooleanField, RadioField,\
+    SubmitField, FieldList, FormField, HiddenField, IntegerField
 from wtforms.validators import DataRequired, Email
+
+class DivisionForm(FlaskForm):
+    level = HiddenField('Level',default=1)
+    title = StringField('Division')
 
 class AuthorForm(FlaskForm):
     author_name = StringField('Name',validators=[DataRequired()])
@@ -12,8 +17,18 @@ class NewProjectForm(FlaskForm):
     project_title = StringField('Title',validators=[DataRequired()])
     project_subtitle = StringField('Subtitle')
     project_edition = StringField('Edition')
+    project_date = StringField('Date')
     project_copyright_year = StringField('Copyright Year')
     project_copyright_holder = StringField('Copyright Holder')
     project_license = RadioField('License')
+    project_has_parts = BooleanField('My book has Parts')
     authors = FieldList(FormField(AuthorForm),min_entries=1)
+    divisions = FieldList(FormField(DivisionForm),min_entries=1)
+    project_chunk = RadioField(
+        'One file per',
+        choices=[
+        ('chapter','Chapter'),
+        ('section','Section'),
+        ('subsection','Subsection')
+    ])
     submit = SubmitField('Generate')
--- pretext_init/views.py
+++ pretext_init/views.py
@@ -12,15 +12,19 @@ def index():
 
 @app.route('/newproject',  methods=['GET', 'POST'])
 def project_file():
-    form = NewProjectForm(authors=[{'name': 'Author 1'},{'name': 'Author 2'}])
+    form = NewProjectForm(
+            authors=[{'name': 'Author 1'},{'name': 'Author 2'}],
+            divisions=[{} for i in range(1,5)]
+        )
     form.project_license.choices = [(e['identifier'],e['title']) for e in app.config['LICENSES']]
     if form.validate_on_submit():
         # process form
         project = ptx_project.from_form(form)
-        yaml = ptx_project.to_xml(project)
+        xml = ptx_project.to_xml(project)
         return Response(
-            yaml,
-            mimetype="text/xml",
-            headers={"Content-disposition":"attachment; filename=project.xml"})
+            xml,
+            mimetype="text/xml"
+            #headers={"Content-disposition":"attachment; filename=project.xml"}
+        )
     # if the form hasn't been submitted and validated, render and serve it instead
     return render_template('new_project.html', title='Initialize Project', form=form)
