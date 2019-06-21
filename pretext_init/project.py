6f8fdfe9bb269ffb7fd3939d8957bee476dba957
Matthew Leingang <leingang@nyu.edu>
2019-06-20 20:42:05 -0700

    Implement outline processing to tree
    I feel like a CS 101 student

--- pretext_init/project.py
+++ pretext_init/project.py
@@ -43,12 +43,17 @@ def from_form(form):
         license=form.project_license.data,
         authors=authors
     )
+    project.outline = [
+        (division_form.title.data, int(division_form.level.data))
+        for division_form in form.divisions
+    ]
     return project
 
 def to_xml(project):
     """Serialize a project as a string of XML"""
     root = etree.Element('book')
-    root.set('{http://www.w3.org/XML/1998/namespace}id','main')
+    XML_ID = '{http://www.w3.org/XML/1998/namespace}id'
+    root.set(XML_ID,'main')
     metadata = etree.SubElement(root,'metadata')
     title = etree.SubElement(metadata,'title')
     title.text = project.title
@@ -70,7 +75,37 @@ def to_xml(project):
             author_children['field'] = etree.SubElement(author_element,field)
             author_children['field'].text = author[field]
         metadata.append(author_element)
-    return etree.tostring(root,pretty_print=True)
+    outline = etree.SubElement(root,'outline')
+    labels=[('chapter','chap'),('section','sec'),('subsection','subsec')]
+    current_level = 0
+    current_element = outline
+    level_stack = []
+    def get_id(short,lst):
+        return short + '-'.join(str(i) for i in lst)
+    for (title,level) in project.outline:
+        tag,short_tag = labels[level-1]
+        element = etree.Element(tag)
+        if (level > current_level):            
+            # add as a child
+            parent = current_element
+            level_stack.append(1)
+        else:
+            # add as a sibling or ancestor
+            # the range starts from 0, so if level = current_level, we go up
+            # exactly once.  The new element's parent is the same as the
+            # current element's parent.  Otherwise, we go up a few more times
+            # on the tree.
+            for _ in range(current_level - level+1):
+                current_element = current_element.getparent()
+                v = level_stack.pop()
+            level_stack.append(v+1)
+            parent = current_element
+        element.set(XML_ID,get_id(short_tag,level_stack))
+        etree.SubElement(element,'title').text = title
+        parent.append(element)
+        current_level = level
+        current_element = element
+    return etree.tostring(root,xml_declaration=True,pretty_print=True)
 
 def to_yaml(project):
     """Serialize a project as a string of YAML
